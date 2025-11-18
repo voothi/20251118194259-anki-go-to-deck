9707868ea007e7528af12ff2c0fcece729dbcfe6
Denis Novikov <voothi@gmail.com>
2025-11-18 20:19:48 +0100

    20251118201947

--- anki_go_to_deck.py
+++ anki_go_to_deck.py
@@ -1,5 +1,33 @@
 from aqt import mw
 from aqt.gui_hooks import browser_will_show_context_menu
+from aqt.utils import tooltip
+from aqt.qt import QTreeWidgetItemIterator
+
+def expand_and_select_legacy(browser, deck_name):
+    try:
+        tree = browser.form.searchTree
+    except AttributeError:
+        return False
+
+    iterator = QTreeWidgetItemIterator(tree)
+    while iterator.value():
+        item = iterator.value()
+        if item.text(0) == deck_name:
+            
+            parent = item.parent()
+            while parent:
+                parent.setExpanded(True)
+                parent = parent.parent()
+            
+            tree.setCurrentItem(item)
+            item.setSelected(True)
+            tree.scrollToItem(item)
+            
+            return True
+        
+        iterator += 1
+    
+    return False
 
 def filter_by_card_deck(browser):
     cids = browser.selectedCards()
@@ -10,6 +38,11 @@ def filter_by_card_deck(browser):
     deck_name = mw.col.decks.name(card.did)
     
     browser.setFilter(f'deck:"{deck_name}"')
+    
+    success_legacy = expand_and_select_legacy(browser, deck_name)
+    
+    if not success_legacy:
+        tooltip(f"📂 Открыта колода:\n{deck_name}")
 
 def on_context_menu(browser, menu):
     action = menu.addAction("Go to Deck")
